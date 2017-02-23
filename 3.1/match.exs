420b004b8ff735e9e206a97280d53d2196da70ba
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2017-02-23 01:14:57 -0300

    Lesson 3

---
+++ 3.1/match.exs
@@ -0,0 +1,25 @@
+IO.puts foo = 4
+
+"José " <> surname = "José de Los Vegetales"
+IO.puts surname
+
+data = ["Elixir", "Potato"]
+IO.puts data
+
+
+
+
+
+[lang, food] = data
+
+IO.puts "#{lang}, #{food}"
+
+
+
+
+
+withdrawals = [50.0, 60.75, 12.0]
+
+[gym, cell_phone, netflix] = withdrawals
+
+IO.puts "Gym: $#{gym}, Cell Phone: $#{cell_phone}, Netflix: $#{netflix}"
